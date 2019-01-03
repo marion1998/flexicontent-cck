69b06798c3edddea28139a2ae60765f4afcc8663
marion1998 <marion281098@outlook.fr>
2019-01-03 09:43:55 +0100

    replace file helper

--- modules/mod_flexigooglemap/helper.php
+++ modules/mod_flexigooglemap/helper.php
@@ -150,7 +150,7 @@ class modFlexigooglemapHelper
 					$linkdirection= '<div class="directions"><a href="' . $map_link . '" target="_blank" class="direction">' . $directionname . '</a></div>';
 				}
 
-				$contentwindows = $infotextmode
+				$contentwindows = $infotextmode //données venant d'un text area du type html + shortcode (your text {{text-2}}) venant de la fonction createDisplayHTML()
 					? $relitem_html
 					: $addr . ' ' . $link;
 
@@ -282,4 +282,202 @@ class modFlexigooglemapHelper
 		}
 	}
 
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// Helper method to create HTML display of an item list according to replacements
+		public function createDisplayHTML($field, $item, $grouped_fields, $infotextmode, $max_count, $pretext, $posttext)
+		{
+			//return array('"<b>Custom HTML</b>" display for fieldgroup field, is not implemented yet, please use default HTML');
+	
+			if (!$infotextmode)
+			{
+				return "Empty custom HTML variable for group field: ". $field->label;
+			}
+	
+	
+			/**
+			 * Parse and identify custom fields
+			 */
+	
+			$result = preg_match_all("/\{\{([a-zA-Z_0-9-]+)(##)?([a-zA-Z_0-9-]+)?\}\}/", $infotextmode, $field_matches);
+			$gf_reps    = $result ? $field_matches[0] : array();
+			$gf_names   = $result ? $field_matches[1] : array();
+			$gf_methods = $result ? $field_matches[3] : array();
+	
+			/*foreach ($gf_names as $i => $gf_name)
+			{
+				$parsed_fields[] = $gf_names[$i] . ($gf_methods[$i] ? "->". $gf_methods[$i] : "");
+			}
+			echo "$contentwindows :: Fields for Related Items List: ". implode(", ", $parsed_fields ? $parsed_fields : array() ) ."<br/>\n";*/
+	
+			
+			//Definition du nom des champs
+			$_name_to_field = array($grouped_fields);
+			foreach($grouped_fields as $i => $grouped_field)
+			{
+				$_name_to_field[$grouped_field->name] = $grouped_fields[$i];
+			}
+			print_r(array_keys($_name_to_field)); echo "<br/>";
+	
+	
+			/**
+			 * Replace ITEM properties
+			 */
+	
+			preg_match_all("/{item->([0-9a-zA-Z_]+)}/", $infotextmode, $matches);
+	
+			foreach ($matches[0] as $i => $replacement_tag)
+			{
+				$prop_name = $matches[1][$i];
+				if (isset($item->{$prop_name}))
+				{
+					$infotextmode = str_replace($replacement_tag, $item->{$prop_name}, $infotextmode);
+				}
+			}
+	
+	
+			/**
+			 * Replace language strings
+			 */
+	
+			$result = preg_match_all("/\%\%([^%]+)\%\%/", $infotextmode, $translate_matches);
+			$translate_strings = $result ? $translate_matches[1] : array('FLEXI_READ_MORE_ABOUT');
+	
+			foreach ($translate_strings as $translate_string)
+			{
+				$infotextmode = str_replace('%%'.$translate_string.'%%', JText::_($translate_string), $infotextmode);
+			}
+	
+	
+			/**
+			 * Render and Replace HTML display of fields
+			 */
+	
+			$_rendered_fields = array();
+			if ( count($gf_names) )
+			{
+				$app = JFactory::getApplication();
+				$view = $app->input->get('flexi_callview', $app->input->get('view', 'item', 'cmd'), 'cmd');
+				$gf_props = array();
+				foreach($gf_names as $pos => $grp_field_name)
+				{
+					// Check that field exists and is assigned the fieldgroup field (needed only when using custom fieldgroup display HTML)
+					if (!isset($_name_to_field[$grp_field_name]))
+					{
+						continue;
+					}
+	
+					// Check that field is assigned to the content type
+					if (!isset($item->fields[$grp_field_name]))
+					{
+						continue;
+					}
+	
+					$_grouped_field = $_name_to_field[$grp_field_name];
+	
+					// Get item's field object, set 'value' and 'ingroup' properties
+					$grouped_field = $item->fields[$_grouped_field->name];
+					$grouped_field->value = $_grouped_field->value;
+					$grouped_field->ingroup = 1;  // render as array
+					$_values = null;
+					$_rendered_fields[$pos] = $grouped_field;
+	
+					// Check if display method is 'label' aka nothing to render
+					if ( $gf_methods[$pos] == 'label' ) continue;
+	
+					// Get custom display method (optional)
+					$method = $gf_methods[$pos] ? $gf_methods[$pos] : 'display';
+	
+					// Backup display method of the field in cases it is displayed outside of fieldgroup too
+					if (isset($grouped_field->$method))
+					{
+						$grouped_field->{$method.'_non_arr'} = $grouped_field->$method;
+						unset($grouped_field->$method);
+					}
+	
+					// SAME field with SAME method, may have been used more than ONCE, inside the custom HTML parameter, so check if field has been rendered already
+					if ( isset($grouped_field->{$method.'_arr'}) && is_array($grouped_field->{$method.'_arr'}) ) continue;
+	
+					// Render the display method for the given field
+					//echo 'Rendering: '. $grouped_field->name . ', method: ' . $method . '<br/>';
+					//FLEXIUtilities::call_FC_Field_Func($grouped_field->field_type, 'onDisplayFieldValue', array(&$grouped_field, $item, $_values, $method));
+					FlexicontentFields::renderField($item, $grouped_field, $_values, $method, $view, $_skip_trigger_plgs = true);  // We will trigger only once the final result
+	
+					// Set custom display variable of field inside group
+					$grouped_field->{$method.'_arr'} = isset($grouped_field->$method) ? $grouped_field->$method : null;
+					unset($grouped_field->$method);
+					unset($grouped_field->ingroup);
+	
+					// Restore non-fieldgroup display of the field
+					if (isset($grouped_field->{$method.'_non_arr'}))
+					{
+						$grouped_field->$method = $grouped_field->{$method.'_non_arr'};
+						unset($grouped_field->{$method.'_non_arr'});
+					}
+				}
+			}
+	
+	
+			/**
+			 * Render the value list of the fieldgroup, using custom HTML for each
+			 * value-set of the fieldgroup, and performing the field replacements
+			 */
+	
+			// Get labels to hide on empty values
+			// $hide_lbl_ifnoval = $this->getHideLabelsOnEmpty($field);
+	
+			$custom_display = array();
+			//echo "<br/>max_count: ".$max_count."<br/>";
+			for ($n = 0; $n < $max_count; $n++)
+			{
+				$rendered_html = $infotextmode;
+				foreach($_rendered_fields as $pos => $_rendered_field)
+				{
+					$method = $gf_methods[$pos] ? $gf_methods[$pos] : 'display';
+	
+					//echo 'Replacing: '. $_rendered_field->name . ', method: ' . $method . ', index: ' .$n. '<br/>';
+					if ($method !== 'label' && $method !== 'id' && $method !== 'name')
+					{
+						$_html = isset($_rendered_field->{$method.'_arr'}[$n]) ? $_rendered_field->{$method.'_arr'}[$n] : '';
+					}
+	
+					// Skip (hide) label for field having none display HTML (is such behaviour was configured)
+					elseif ($method === 'label')
+					{
+						$_html = isset($hide_lbl_ifnoval[$_rendered_field->id])  &&  (!isset($_rendered_field->{$method.'_arr'}) || !isset($_rendered_field->{$method.'_arr'}[$n]) || !strlen($_rendered_field->{$method.'_arr'}[$n]))
+							? ''
+							: $_rendered_field->label;
+					}
+	
+					// id (and in future other properties ?)
+					else
+					{
+						$_html = ! in_array($method, array('id', 'name'))
+							? ''
+							: $_rendered_field->{$method};
+					}
+	
+					$rendered_html = str_replace($gf_reps[$pos], $_html, $rendered_html);
+				}
+	
+				// Replace value position
+				$rendered_html = str_replace('{{value##count}}', $n, $rendered_html);
+	
+				$custom_display[$n] = $pretext . $rendered_html . $posttext;
+			}
+	
+			return $custom_display;
+		}
 }
